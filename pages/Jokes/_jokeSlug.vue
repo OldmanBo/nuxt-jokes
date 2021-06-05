71b1cf96d8614e304dfd606f8f2b2f9787df59b1
OldmanBo <76737153+OldmanBo@users.noreply.github.com>
2021-06-05 14:07:19 +0200

    Replaced vuejs method for route validation with the nuxt method.

--- pages/Jokes/_jokeSlug.vue
+++ pages/Jokes/_jokeSlug.vue
@@ -9,20 +9,30 @@ import axios from "axios"
 import JokeContainer from '../../components/JokeContainer.vue'
 
 export default {
-  beforeRouteEnter: (to, from, next) => {
-    axios.get(`https://icanhazdadjoke.com/j/${to.params.jokeSlug}`, {headers: {'Accept': 'application/json'}})
-    .then(res => {
-      if(res.data.status === 404) {
-        console.log(to.params.jokeSlug + ' ' + 'ERRORs')
-        next({name: 'NotFound'})
-      } else {
-        console.log(res.data + ' ' + to.params.jokeSlug + ' ' + 'success')
-        next()
-      }
-    })
-    .catch(err => {
-      console.log('ERROR ' + err + ' ' + res.data + ' ' + to.params.jokeSlug)
-    })
+
+  // ----This is a Vue.js method for dynamic route navigation, but nuxt has the validate() method for that purpose----
+
+  // beforeRouteEnter: (to, from, next) => {
+  //   axios.get(`https://icanhazdadjoke.com/j/${to.params.jokeSlug}`, {headers: {'Accept': 'application/json'}})
+  //   .then(res => {
+  //     if(res.data.status === 404) {
+  //       console.log(to.params.jokeSlug + ' ' + 'ERRORs')
+  //       next({name: 'NotFound'})
+  //     } else {
+  //       console.log(res.data + ' ' + to.params.jokeSlug + ' ' + 'success')
+  //       next()
+  //     }
+  //   })
+  //   .catch(err => {
+  //     console.log('ERROR ' + err + ' ' + res.data + ' ' + to.params.jokeSlug)
+  //   })
+  // },
+
+  async validate(context) {
+    const res = await axios.get(`https://icanhazdadjoke.com/j/${context.route.params.jokeSlug}`, {headers: {'Accept': 'application/json'}})
+    // console.log(res.data.status)
+    // console.log(context.route.params.jokeSlug)
+    return res.data.status === 200
   },
   components: {JokeContainer},
   data() {
